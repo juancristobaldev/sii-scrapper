b87b2dfb5ed0418732cfac4f1c1805cad459373e
Juan Cristobal <juancristobal@Juans-MacBook-Pro.local>
2026-06-19 11:22:04 -0400

    fix

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css'
 import { AuthProvider } from '@/lib/context/auth-context'
 
 const inter = Inter({ subsets: ['latin'] })
-
+//
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es" className="dark">
